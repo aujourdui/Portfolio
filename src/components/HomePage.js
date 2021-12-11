661510a20ec9bba94c76a06e601984b3767291e7
kyosuke ito <kyosuke.ito@gmail.com>
2021-12-12 08:36:05 +0900

    ajust styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => (
       <div className="image-vancouver">
         <div className="introduce__container">
           <h1 className="name">Kyosuke Ito</h1>
-          <p>I'm a front-end developer in Vancouver</p>
+          <p className="introduce">I'm a front-end developer in Vancouver</p>
         </div>
       </div>
     </div>
