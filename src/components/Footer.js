441c1c1c6e8bce9d11e8a814ca2f9f1eebad203f
kyosuke ito <kyosuke.ito@gmail.com>
2021-12-19 14:02:32 +0900

    styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,18 @@ import React from "react";
 const Footer = () => (
   <div className="footer">
     <h1 className="footer-title">Contact</h1>
-    {/* <div>(236)833-8229</div>
-        <div>kyosuke0215@gmail.com</div> */}
-    <div>
-      <a href="https://github.com/aujourdui?tab=repositories">Github</a>
-    </div>
-    <div>
-      <a href="https://www.linkedin.com/in/kyosuke-ito-506090a8/">
-        Linkdin
-      </a>
-    </div>
-    <div className="bottom-item">
-      <a href="https://medium.com/@kyosuke0215">Medium</a>
+    <div className="footer-contact">(236)833-8229</div>
+    <div className="footer-contact">kyosuke0215@gmail.com</div>
+    <div className="footerLink__wrapper">
+      <div className="footer-link">
+        <a href="https://github.com/aujourdui?tab=repositories">Github</a>
+      </div>
+      <div className="footer-link">
+        <a href="https://www.linkedin.com/in/kyosuke-ito-506090a8/">Linkdin</a>
+      </div>
+      <div className="bottom-item footer-link">
+        <a href="https://medium.com/@kyosuke0215">Medium</a>
+      </div>
     </div>
   </div>
 );
