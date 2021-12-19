6de410a1763fd9c7f1915e2405c017dfb2ace94f
kyosuke ito <kyosuke.ito@gmail.com>
2021-12-19 11:07:13 +0900

    add project detail image

--- src/components/project/ProjectDetail.js
+++ src/components/project/ProjectDetail.js
@@ -4,21 +4,25 @@ import { useParams } from "react-router-dom";
 const ProjectDetail = ({ data }) => {
   const { title } = useParams();
   const linkPathCode = "https://github.com/aujourdui/";
+  const baseImgPath = "/images/";
   return (
-    <div>
+    <div className="projectDetail__container">
       <div>
         {data
           .filter((list) => list.title === title)
           .map((list) => (
-            <div key={list.id}>
-              <h1>{list.title}</h1>
-              <h2>{list.article}</h2>
-              <p>Technology: {list.technology}</p>
-              <div>
-                <a href={`${linkPathCode}${list.gitTitle}`}>Code</a>
-              </div>
-              <div>
-                <a href={list.app}>{list.appName}</a>
+            <div className="projectDetail__wrapper" key={list.id}>
+              <h1 className="project__content">{list.title}</h1>
+              <h2 className="project__content">{list.article}</h2>
+              <img src={`${baseImgPath}${list.imgDetailPath}`} alt="Project" />
+              <p className="project__content">Technology: {list.technology}</p>
+              <div className="detailLink__wrapper">
+                <div className="project__content">
+                  <a href={`${linkPathCode}${list.gitTitle}`}>Code</a>
+                </div>
+                <div className="project__content">
+                  <a href={list.app}>{list.appName}</a>
+                </div>
               </div>
             </div>
           ))}
