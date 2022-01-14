250b391f5c71ecf8e6180ac4be9c812fff05a0a5
kyosuke ito <kyosuke.ito@gmail.com>
2022-01-14 12:03:33 -0800

    add css animation

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { resumeDataList } from "../../resource/data";
 
-const HomePage = () => (
-  <div className="home">
-    <div className="image-vancouver">
-      <div className="introduce__container">
-        <h1 className="name">{resumeDataList[0].name}</h1>
-        <p className="introduce">{resumeDataList[0].intro}</p>
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const onLoad = () => {
+    // setTimeout(() => setIsLoading(false), 3000);
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="home">
+      <div
+        className="image-vancouver"
+        // style={{ display: isLoading ? "none" : "block" }}
+        // onLoad={onLoad}
+      >
+        {/* <div
+        className="image-placeholder"
+        style={{ display: isLoading ? "block" : "none" }}
+        onLoad={onLoad}
+      > */}
+        <div className="introduce__container">
+          <h1 className="name">{resumeDataList[0].name}</h1>
+          <p className="introduce">{resumeDataList[0].intro}</p>
+        </div>
       </div>
+      {/* </div> */}
     </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
