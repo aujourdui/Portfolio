fe6dc0d1f6255fb9ae5ac13feee96df0a62b3bc8
kyosuke ito <kyosuke.ito@gmail.com>
2022-01-19 12:40:43 -0800

    change latest to add feature cursor context

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import MouseContextProvider from "./context/mouse-context";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+ReactDOM.render(
+  <MouseContextProvider>
+    <AppRouter />
+  </MouseContextProvider>,
+  document.getElementById("app")
+);
