13245b38693e69c25b9d55b32d1c2f30f86295ab
kyosuke ito <kyosuke.ito@gmail.com>
2022-01-20 09:17:10 -0800

    introduce basic context for cursor and example

--- src/components/feature/Cursor.tsx
+++ src/components/feature/Cursor.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import useMousePosition from "./useMousePosition";
+import { MouseContext } from "../../context/mouse-context";
 
 const Cursor = () => {
+  const { cursorType } = useContext(MouseContext);
+
   const { x, y } = useMousePosition();
   return (
     <>
-      <div style={{ left: `${x}px`, top: `${y}px` }} className="ring"></div>
-      <div className="dot" style={{ left: `${x}px`, top: `${y}px` }}></div>
+      <div
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className={"ring " + cursorType}
+      ></div>
+      <div
+        className={"dot " + cursorType}
+        style={{ left: `${x}px`, top: `${y}px` }}
+      ></div>
     </>
   );
 };
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
@@ -9,13 +9,23 @@ import ResumePage from "../components/pages/ResumePage";
 import NotFoundPage from "../components/pages/NotFoundPage";
 import Cursor from "../components/feature/Cursor";
 import data from "../resource/data";
+import { MouseContext } from "../context/mouse-context";
 
 const AppRouter = () => {
+  const { cursorChangeHandler } = useContext(MouseContext);
   return (
     <BrowserRouter>
       <>
         <Cursor />
         <Header />
+        <div>
+          <div
+            onMouseEnter={() => cursorChangeHandler("hovered")}
+            onMouseLeave={() => cursorChangeHandler("")}
+          >
+            <h1>Hover over me</h1>
+          </div>
+        </div>
         <Switch>
           <Route path="/" component={HomePage} exact={true} />
           <Route path="/project" exact={true}>
